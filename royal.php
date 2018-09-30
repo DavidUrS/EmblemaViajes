ae011b05b0726c766fd027fdcc79d46aa62ec757
David Uribe <duribe@nativapps.co>
2018-09-30 17:59:54 -0500

    add royal

--- royal.php
+++ royal.php
@@ -21,9 +21,9 @@
     include_once 'headerBlack.php';
 ?>
 
-<h1 class="display-3 text-center mt-1">Descubre Royal Concierge</h1>
+<h1 class="display-4 text-center mt-1">Descubre Royal Concierge</h1>
 <hr>
-<p class="lead">¿Qué es Royal Concierge?</p>
+<p class="lead text-right">¿Qué es Royal Concierge?</p>
 <p>
 Royal Concierge es el concierge virtual que ofrece el servicio personalizado que prestaba antaño la figura emblemática del Conserje Hotelero que poco a poco se fue eliminando y desapareciendo y que ha sobrevivido únicamente en hoteles de gran lujo y marcas prestigiosas.
 <br>
@@ -32,7 +32,13 @@ Royal Concierge atiende a sus afiliados y socios de la misma manera que un “Co
 Sus afiliados y socios participan además del programa RC LOYALTY PROGRAM y de todos los beneficios que obtienen al usar el servicio de Royal Concierge.
 </p>
 <p></p>
-<p class="lead">¿Quién puede inscribirse?</p>
+<hr>
+<p class="lead text-right">¿Quién puede inscribirse?</p>
+<h4>Royal Concierge BUTLER</h4>
+<em>Quien puede inscribirse:</em>
+<p>
+Toda persona particular en Colombia que desee disfrutar de un servicio de consulta, soporte y gestión en materia de actividades, viajes, alojamientos, restaurantes, compras, eventos, servicios del día a día, ocio y muchos otros servicios.
+</p>
 
 <?php
     include_once 'footer.php';
