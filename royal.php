b1f4c6129a3603449c799c0e967f3ad5dc7e969d
David Uribe <duribe@nativapps.co>
2018-09-30 19:13:31 -0500

    vamos pa lante con royal

--- royal.php
+++ royal.php
@@ -22,16 +22,26 @@
 ?>
 <div class="container">
     <h1 class="display-4 text-center mt-1">Descubre Royal Concierge</h1>
-    <img width="14%" class="mx-auto d-block" src="./img/royal/royal.jpeg" alt="">
+    <img width="15%" class="mx-auto d-block" src="./images/19.jpg" alt="">
     <hr>
     <p class="lead text-center">¿Qué es Royal Concierge?</p>
-    <p>
-    Royal Concierge es el concierge virtual que ofrece el servicio personalizado que prestaba antaño la figura emblemática del Conserje Hotelero que poco a poco se fue eliminando y desapareciendo y que ha sobrevivido únicamente en hoteles de gran lujo y marcas prestigiosas.
-    <br>
-    Royal Concierge atiende a sus afiliados y socios de la misma manera que un “Concierge llaves de oro”, trato personalizado, especializado, experto y eficiente ante cualquier consulta, gestión o averiguación. 
-    <br>
-    Sus afiliados y socios participan además del programa RC LOYALTY PROGRAM y de todos los beneficios que obtienen al usar el servicio de Royal Concierge.
-    </p>
+    <div class="row">
+      <div class="col-md-9">
+        <p>
+        Royal Concierge es el concierge virtual que ofrece el servicio personalizado que prestaba antaño la figura emblemática del Conserje Hotelero que poco a poco se fue eliminando y desapareciendo y que ha sobrevivido únicamente en hoteles de gran lujo y marcas prestigiosas.
+        <br>
+        Royal Concierge atiende a sus afiliados y socios de la misma manera que un “Concierge llaves de oro”, trato personalizado, especializado, experto y eficiente ante cualquier consulta, gestión o averiguación. 
+        <br>
+        Sus afiliados y socios participan además del programa RC LOYALTY PROGRAM y de todos los beneficios que obtienen al usar el servicio de Royal Concierge.
+        <br>
+        <a href="#">PDF – Brochure descargable</a>
+        </p>
+      </div>
+      <div class="col-md-3">
+        <img class="rounded mx-auto d-block" width="40%" src="./img/royal/royal.jpeg" alt="">
+      </div>
+    </div>
+    
     <p></p>
     <hr>
     <p class="lead text-center">¿Quién puede inscribirse?</p>
@@ -44,10 +54,10 @@
     <form id="form_contactanos">
           <div class="row">
             <div class="col">
-              <input type="text" class="form-control" placeholder="Nombres" id="nombres">
+              <input type="text" class="form-control" placeholder="Nombres y Apellidos" id="nombres">
             </div>
             <div class="col">
-              <input type="text" class="form-control" placeholder="Apellidos" id="apellidos">
+              <input type="text" class="form-control" placeholder="Email" id="apellidos">
             </div>
           </div>
           <div class="row mt-2">
@@ -96,10 +106,10 @@
 <form id="form_contactanos">
           <div class="row">
             <div class="col">
-              <input type="text" class="form-control" placeholder="Nombres" id="nombres">
+              <input type="text" class="form-control" placeholder="Nombres y Apellidos" id="nombres">
             </div>
             <div class="col">
-              <input type="text" class="form-control" placeholder="Apellidos" id="apellidos">
+              <input type="text" class="form-control" placeholder="Email" id="apellidos">
             </div>
           </div>
           <div class="row mt-2">
